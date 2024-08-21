a188785ab2074bfdb8dee78f38e2d4039d1d5582
KenBlessing <fredkens83@gmail.com>
2024-08-21 15:06:43 +0300

    passed changes on my second repo

---
+++ Day3 work.py
@@ -0,0 +1,11 @@
+# My day 3 work
+print("Hello worl")
+# Get the base and height of the triangle from the user.
+base = 5
+height = 4
+
+# Compute the area of the triangle using base and height
+area = 0.5 * base * height
+
+# Print the area of the triangle with two decimal places
+print("The area of the triangle is", "{:.2f}".format(area), "square centimeters")
